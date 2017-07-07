269bb623c16e879a3acaefc6706cc3e01a67ba57
robinarthur <stukka@gmx.de>
2017-07-07 15:17:22 +0200

    logger
    logger wurde erweitert. Error in stream + logdatei

--- logger.py
+++ logger.py
@@ -4,19 +4,29 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-#create console handler and set level to DEBUG
+#log File path
+f = 'log/logfile.txt'
+
+# create file handler which logs even debug messages
+fh = logging.FileHandler(f)
+fh.setLevel(logging.DEBUG)
+
+#create console handler with a higher loglevel
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.ERROR)
 
-#create formatter
+#create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-#add formatter to ch
 ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+# add the handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
 
 #'application' code
-#logger.debug('debug message')
-#logger.info('info message')
-#logger.warn('warn message')
-#logger.error('error message')
-#logger.critical('critical message')
+logger.debug('debug message')
+logger.info('info message')
+logger.warn('warn message')
+logger.error('error message')
+logger.critical('critical message')
--- myapp.py
+++ myapp.py
@@ -2,8 +2,12 @@ import logging
 import logger as lr
 
 def main():
-    lr.logger.info('test test test')
-
+    #logger.error('test test test ERROR')
+    lr.logger.debug('debug message')
+    lr.logger.info('info message')
+    lr.logger.warn('warn message')
+    lr.logger.error('error message')
+    lr.logger.critical('critical message')
 
 if __name__ == '__main__':
     main()
